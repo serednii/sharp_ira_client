9e64fe81571aebe972eb07ef82854c12425bdcbb
Serednii Mykola <seredniimykola@gmail.com>
2024-10-24 00:27:45 +0200

    Updates

--- js/index.js
+++ js/index.js
@@ -115,74 +115,6 @@ const initProgress = async (idQuery) => {
 
 
 //робочий варіант неміняти
-document.getElementById('uploadForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const imageInput = document.getElementById('imageInput').files;
-    const resultImagesDiv = document.getElementById('resultImages');
-    const form = document.getElementById("uploadForm")
-    const formData = new FormData(form);
-
-    resultImagesDiv.innerHTML = ''; // Очищуємо попередні результати
-
-    idQuery = Math.floor(100000 + Math.random() * 900000)
-    console.log(idQuery)
-
-    controller = new AbortController();
-    formData.append('name', imageInput[0].name);
-    formData.append('idQuery', idQuery);
-
-    const res = await initProgress(idQuery)
-    checkProcessingStatus(idQuery)
-
-    console.log(formData)
-
-
-
-    try {
-        // Відправка даних на сервер через fetch
-        console.log('strat strat strat strat strat strat strat strat ')
-        console.log('strat strat strat strat strat strat strat strat ')
-        console.log('strat strat strat strat strat strat strat strat ')
-
-
-        const response = await fetch(`${urlWorkServer}/upload-multiple`, {
-            method: 'POST',
-            body: formData,
-            signal: controller.signal, // Додаємо сигнал скасування
-        });
-
-        if (!response.ok) {
-            throw new Error('Помилка під час завантаження зображень');
-        }
-
-        // Отримання оброблених зображень як Blob
-        const blobs = await response.json();
-        console.log('blobs blobs blobs blobs blobs blobs blobs ')
-        console.log('blobs blobs blobs blobs blobs blobs blobs ')
-        console.log('blobs blobs blobs blobs blobs blobs blobs ')
-
-
-        statusDownloading.innerText = "Done"
-        downloadArchive.href = blobs.downloadLink;
-        downloadArchive.style.display = "inline-block"
-
-        blobs.processedImages.forEach((blobUrl) => {
-            console.log(blobUrl)
-            const li = document.createElement('li'); // Створюємо елемент списку
-            const img = document.createElement('img'); // Створюємо елемент зображення
-            img.src = blobUrl[0].imageBase64;
-            img.alt = 'Оброблене зображення';
-            img.style.maxWidth = '300px'; // Задаємо розмір для відображення
-            li.appendChild(img); // Вставляємо зображення у список
-            resultImagesDiv.appendChild(li); // Додаємо елемент списку у UL
-
-        });
-    } catch (error) {
-        console.error('Сталася помилка:', error);
-    }
-});
-
-//на xhr з контролем загрузки і вигрузки даних
 // document.getElementById('uploadForm').addEventListener('submit', async (e) => {
 //     e.preventDefault();
 //     const imageInput = document.getElementById('imageInput').files;
@@ -211,64 +143,132 @@ document.getElementById('uploadForm').addEventListener('submit', async (e) => {
 //         console.log('strat strat strat strat strat strat strat strat ')
 //         console.log('strat strat strat strat strat strat strat strat ')
 //         console.log('strat strat strat strat strat strat strat strat ')
-//         const xhr = new XMLHttpRequest();
-//         xhr.open('POST', `${urlWorkServer}/upload-multiple`, true);
-//         // Відстеження прогресу завантаження на сервер
-//         xhr.upload.addEventListener('progress', (event) => {
-//             if (event.lengthComputable) {
-//                 percentDownloading = parseInt(((event.loaded / event.total) * 100));
-//                 statusDownloading.innerText = `${downloadStatus}  ${percentDownloading}%`;
-//             }
-//         });
-//         // Відправка даних на сервер
 
-//         xhr.addEventListener('progress', (event) => {
-//             if (event.lengthComputable) {
-//                 percentDownloading = parseInt(((event.loaded / event.total) * 100));
-//                 statusDownloading.innerText = `${downloadStatus}  ${percentDownloading} %`;
-//             }
+
+//         const response = await fetch(`${urlWorkServer}/upload-multiple`, {
+//             method: 'POST',
+//             body: formData,
+//             signal: controller.signal, // Додаємо сигнал скасування
 //         });
 
-//         xhr.onload = () => {
-//             if (xhr.status === 200) {
-//                 const blobs = JSON.parse(xhr.response)
-//                 console.log(blobs)
-//                 console.log('blobs blobs blobs blobs blobs blobs blobs ')
-//                 console.log('blobs blobs blobs blobs blobs blobs blobs ')
-//                 console.log('blobs blobs blobs blobs blobs blobs blobs ')
-
-
-//                 statusDownloading.innerText = "Done"
-//                 downloadArchive.href = blobs.downloadLink;
-//                 downloadArchive.style.display = "inline-block"
-
-//                 blobs.processedImages.forEach((blobUrl) => {
-//                     console.log(blobUrl)
-//                     const li = document.createElement('li'); // Створюємо елемент списку
-//                     const img = document.createElement('img'); // Створюємо елемент зображення
-//                     img.src = blobUrl[0].imageBase64;
-//                     img.alt = 'Оброблене зображення';
-//                     img.style.maxWidth = '300px'; // Задаємо розмір для відображення
-//                     li.appendChild(img); // Вставляємо зображення у список
-//                     resultImagesDiv.appendChild(li); // Додаємо елемент списку у UL
-//                 });
-//                 // const link = document.createElement('a');
-//                 // link.href = URL.createObjectURL(blob);
-//                 // link.download = 'processed_images.zip';
-//                 // link.click();
-//             }
-//         };
-
-
-//         xhr.send(formData);
+//         if (!response.ok) {
+//             throw new Error('Помилка під час завантаження зображень');
+//         }
 
 //         // Отримання оброблених зображень як Blob
+//         const blobs = await response.json();
+//         console.log('blobs blobs blobs blobs blobs blobs blobs ')
+//         console.log('blobs blobs blobs blobs blobs blobs blobs ')
+//         console.log('blobs blobs blobs blobs blobs blobs blobs ')
 
 
+//         statusDownloading.innerText = "Done"
+//         downloadArchive.href = blobs.downloadLink;
+//         downloadArchive.style.display = "inline-block"
+
+//         blobs.processedImages.forEach((blobUrl) => {
+//             console.log(blobUrl)
+//             const li = document.createElement('li'); // Створюємо елемент списку
+//             const img = document.createElement('img'); // Створюємо елемент зображення
+//             img.src = blobUrl[0].imageBase64;
+//             img.alt = 'Оброблене зображення';
+//             img.style.maxWidth = '300px'; // Задаємо розмір для відображення
+//             li.appendChild(img); // Вставляємо зображення у список
+//             resultImagesDiv.appendChild(li); // Додаємо елемент списку у UL
+
+//         });
 //     } catch (error) {
 //         console.error('Сталася помилка:', error);
 //     }
 // });
+
+//на xhr з контролем загрузки і вигрузки даних
+document.getElementById('uploadForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const imageInput = document.getElementById('imageInput').files;
+    const resultImagesDiv = document.getElementById('resultImages');
+    const form = document.getElementById("uploadForm")
+    const formData = new FormData(form);
+
+    resultImagesDiv.innerHTML = ''; // Очищуємо попередні результати
+
+    idQuery = Math.floor(100000 + Math.random() * 900000)
+    console.log(idQuery)
+
+    controller = new AbortController();
+    formData.append('name', imageInput[0].name);
+    formData.append('idQuery', idQuery);
+
+    const res = await initProgress(idQuery)
+    checkProcessingStatus(idQuery)
+
+    console.log(formData)
+
+
+
+    try {
+        // Відправка даних на сервер через fetch
+        console.log('strat strat strat strat strat strat strat strat ')
+        console.log('strat strat strat strat strat strat strat strat ')
+        console.log('strat strat strat strat strat strat strat strat ')
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', `${urlWorkServer}/upload-multiple`, true);
+        // Відстеження прогресу завантаження на сервер
+        xhr.upload.addEventListener('progress', (event) => {
+            if (event.lengthComputable) {
+                percentDownloading = parseInt(((event.loaded / event.total) * 100));
+                statusDownloading.innerText = `${downloadStatus}  ${percentDownloading}%`;
+            }
+        });
+        // Відправка даних на сервер
+
+        xhr.addEventListener('progress', (event) => {
+            if (event.lengthComputable) {
+                percentDownloading = parseInt(((event.loaded / event.total) * 100));
+                statusDownloading.innerText = `${downloadStatus}  ${percentDownloading} %`;
+            }
+        });
+
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                const blobs = JSON.parse(xhr.response)
+                console.log(blobs)
+                console.log('blobs blobs blobs blobs blobs blobs blobs ')
+                console.log('blobs blobs blobs blobs blobs blobs blobs ')
+                console.log('blobs blobs blobs blobs blobs blobs blobs ')
+
+
+                statusDownloading.innerText = "Done"
+                downloadArchive.href = blobs.downloadLink;
+                downloadArchive.style.display = "inline-block"
+
+                blobs.processedImages.forEach((blobUrl) => {
+                    console.log(blobUrl)
+                    const li = document.createElement('li'); // Створюємо елемент списку
+                    const img = document.createElement('img'); // Створюємо елемент зображення
+                    img.src = blobUrl[0].imageBase64;
+                    img.alt = 'Оброблене зображення';
+                    img.style.maxWidth = '300px'; // Задаємо розмір для відображення
+                    li.appendChild(img); // Вставляємо зображення у список
+                    resultImagesDiv.appendChild(li); // Додаємо елемент списку у UL
+                });
+                // const link = document.createElement('a');
+                // link.href = URL.createObjectURL(blob);
+                // link.download = 'processed_images.zip';
+                // link.click();
+            }
+        };
+
+
+        xhr.send(formData);
+
+        // Отримання оброблених зображень як Blob
+
+
+    } catch (error) {
+        console.error('Сталася помилка:', error);
+    }
+});
 
 
 
